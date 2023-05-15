01cfb1da8b54b29088fb86afef5e1127b12cd16d
AaronIzadi <izadiiiw@gmail.com>
2023-05-16 00:02:36 +0330

    Piranha is updated.

--- src/SuperMario/model/enemy/Piranha.java
+++ src/SuperMario/model/enemy/Piranha.java
@@ -4,19 +4,53 @@ import SuperMario.graphic.view.animation.Animation;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Piranha extends Enemy {
 
     private Animation animation;
 
     public Piranha(double x, double y, BufferedImage style) {
-        super(x, y, style);
+        super(x + 22, y, style);
     }
 
     @Override
     public void draw(Graphics g) {
+        if (getY() >= 580) {
+            setY(580);
+            setVelY(0);
+            setTimerToGoUp();
+        }
+        if (getY() <= 480) {
+            setY(480);
+            setVelY(0);
+            setTimerToGoDown();
+        }
         super.draw(g);
         animate();
+    }
+
+    public void setTimerToGoDown() {
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                setVelY(-1);
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(task, 2000);
+    }
+
+    public void setTimerToGoUp() {
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                setVelY(1);
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(task, 3000);
     }
 
     public void animate() {
@@ -32,5 +66,10 @@ public class Piranha extends Enemy {
 
     public void setAnimation(Animation animation) {
         this.animation = animation;
+    }
+
+    @Override
+    public void updateLocation() {
+        super.updateLocation();
     }
 }
